f8c0428360c90ed539f2730ae599a6c95f3904cc
siraegi <shihabbong11@gmail.com>
2019-06-28 16:48:21 +0900

    es6 with typescript setting

--- ICAPP/src/index.ts
+++ ICAPP/src/index.ts
@@ -1,10 +1,10 @@
-var express = require('express');
+import express from 'express';
 
-var app = express();
+const app = express();
 
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', (req, res) => {
     res.send('hello world');
-})
+});
 
 app.listen(8000, () => {
     console.log('server is listening 8000');
