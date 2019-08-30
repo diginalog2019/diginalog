fa724f916d57597215c7ec82b28da0e26ac02695
Kim Juhui <kjh107704@gmail.com>
2019-08-30 21:16:27 +0900

    make 'getAllProducts' component

--- backend/src/controller/UserController.ts
+++ backend/src/controller/UserController.ts
@@ -2,6 +2,8 @@ import {getConnection} from "typeorm";
 import {User} from "../entity/User";
 import {ResultVo} from "../vo/ResultVo";
 import {Category} from "../entity/Category";
+import {Product} from "../entity/Product";
+import {Creator} from "../entity/Creator";
 
 export class UserController {
     static getProduct = async (req, res) => {
@@ -13,32 +15,45 @@ export class UserController {
 
         const result = await getConnection().getRepository(Category).findOne(options);
         res.send(result);
+    }
+    // Kim Ju Hui : 2019.08.30 Fri-------------------------------------------
+    static getAllProduct = async(req,res) => {
 
-        /*const {start_index, page_size} = req.query;
-        console.log("1");
-        const options = {};
-        options['select'] = ["UID","U_Name"];
-        console.log("2");
-        options['order'] = {UID: 'ASC'};
+        const {start_index, page_size} = req.query;
+
+        const options = { relation:["category"],where: { State: 1 }};
+
+        options['select'] = ["PID", "P_Name", "P_Price", "P_StarPoint", "P_TitleIMG","categoryCateID","creatorCID"];
+
         if (start_index) {
             options['skip'] = start_index;
-            console.log("3");
         }
         if (page_size) {
             options['take'] = page_size;
-            console.log("4");
         }
 
-        //options['select'] = ["id", "name", "email", "photo"];
+        const products = await getConnection().getRepository(Product).find(options);
 
-        const heroes = await getConnection().getRepository(User).find(options);
-        console.log("5");
-        const total = await getConnection().getRepository(User).count();
-        console.log("6");
-        const result = new ResultVo(0, "success");
-        console.log("7");
-        result.data = heroes;
+        // Shi Ha Yeon : 2019.08.30 Fri----------------------------------------------------------------------
+        const categorys = await getConnection().getRepository(Category).find();
+        // Kim Ju Hui : 2019.08.30 18:24 Fri-----------------------------------------------------------------------
+        const creators = await getConnection().getRepository(Creator).find();
+
+        const products2 = products.map(product => {
+            let product2 = {CreatorName: creators[(product.creatorCID-1)].C_Nickname,CateName:categorys[(product.categoryCateID-1)].Cate_Name, ...product};
+         // Kim Ju Hui : 2019.08.30 18:24 Fin----------------------------------------------------------------------
+            return product2;
+        });
+
+        // Shi Ha Yeon : 2019.08.30 Fin ---------------------------------------------------------------------
+
+        const total = await getConnection().getRepository(Product).count();
+
+        const result = new ResultVo(0,"success");
+
+        result.data = products2;
         result.total = total;
-        res.send(result);*/
+        res.send(result);
     }
+    // Kim Ju Hui : 2019.08.30 Fin-------------------------------------------
 }
