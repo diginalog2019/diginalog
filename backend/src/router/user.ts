7084724c763db74b789d0fa51733bc4631238f68
Kim Juhui <kjh107704@gmail.com>
2019-08-30 21:15:47 +0900

    modify '/products' component from UserController.getProduct to UserController.getAllProduct

--- backend/src/router/user.ts
+++ backend/src/router/user.ts
@@ -2,7 +2,7 @@ import {Router} from "express";
 import {UserController} from "../controller/UserController"
 const routes = Router();
 
-routes.get('/products', UserController.getProduct);
+routes.get('/products', UserController.getAllProduct);
 routes.get('/product/:Cate_ID([0-9]+)', UserController.getProduct);
 
 export default routes;
