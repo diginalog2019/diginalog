ca96f4a09e15d89f753c3b1a98e05bd5acd02908
Kim Juhui <kjh107704@gmail.com>
2019-09-16 21:24:33 +0900

    modify user router

--- backend/src/router/user.ts
+++ backend/src/router/user.ts
@@ -3,6 +3,7 @@ import {UserController} from "../controller/UserController"
 const routes = Router();
 
 routes.get('/products', UserController.getAllProduct);
-routes.get('/product/:Cate_ID([0-9]+)', UserController.getProduct);
+routes.get('/product/', UserController.getProduct);
+routes.get('/download',UserController.downloadFile);
 
 export default routes;
