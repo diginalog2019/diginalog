40aea225c6d830af2330ae4c91d3c7a7d597a384
Kim Juhui <kjh107704@gmail.com>
2019-10-04 11:51:49 +0900

    modify getAllProduct, getProduct component and add getFileUrl component

--- backend/src/controller/UserController.ts
+++ backend/src/controller/UserController.ts
@@ -4,92 +4,70 @@ import {ResultVo} from "../vo/ResultVo";
 import {Category} from "../entity/Category";
 import {Product} from "../entity/Product";
 import {Creator} from "../entity/Creator";
+import {File} from "../entity/File";
 import {s3} from "../config/aws";
 
 export class UserController {
     // Kim Ju Hui : 2019.09.13 Sat--------------------------------------------
     static getProduct = async (req, res) => {
 
-        console.log(req.query);
         const {id} = req.query;
 
-        console.log("pid = "+id);
+        let product = await getConnection().getRepository(Product).createQueryBuilder("product").where("product.PID = :pid", {pid: id}).leftJoinAndSelect("product.creator","creator").leftJoinAndSelect("product.category","category").leftJoinAndSelect("product.files", "file").getOne();
 
-        const options = {relation:["category"], where: {PID: id}};
-        let product = await getConnection().getRepository(Product).findOne(options);
-
-        const category = await getConnection().getRepository(Category).findOne({where:{Cate_ID:product.categoryCateID}});
-        const creator = await getConnection().getRepository(Creator).findOne({where:{CID:product.creatorCID}});
-
-        product = {...product,category,creator};
-
-        const result = new ResultVo(0,"success");
+        const result = new ResultVo(0, "success");
         result.data = product;
         res.send(result);
     }
     // Kim Ju Hui : 2019.09.13 Sat Fin-------------------------------------------
     // Kim Ju Hui : 2019.08.30 Fri-------------------------------------------
-    static getAllProduct = async(req,res) => {
+    static getAllProduct = async (req, res) => {
 
         const {start_index, page_size} = req.query;
 
-        const options = { relation:["category"],where: { State: 1 }};
+        let products = await getConnection().getRepository(Product).createQueryBuilder("product").where("product.State = :state", {state: 1}).leftJoinAndSelect("product.creator","creator").leftJoinAndSelect("product.category","category").leftJoinAndSelect("product.files", "file").skip(start_index).take(page_size).getMany();
 
-        if (start_index) {
-            options['skip'] = start_index;
-        }
-        if (page_size) {
-            options['take'] = page_size;
-        }
-
-        const products = await getConnection().getRepository(Product).find(options);
-
-        // Shi Ha Yeon : 2019.08.30 Fri-----------------------------------------------------------------------
-        const categorys = await getConnection().getRepository(Category).find();
-        // Kim Ju Hui : 2019.08.30 18:24 Fri-----------------------------------------------------------------------
-        const creators = await getConnection().getRepository(Creator).find();
         const total = products.length;
-        const products2 = products.map(product => {
-            let product2 = {CreatorName: creators[(product.creatorCID-1)].C_Nickname,CateName:categorys[(product.categoryCateID-1)].Cate_Name, ...product};
-            // Kim Ju Hui : 2019.08.30 18:24 Fin----------------------------------------------------------------------
-            return product2;
-        });
-
-        // Shi Ha Yeon : 2019.08.30 Fri Fin ---------------------------------------------------------------------
-
         const result = new ResultVo(0,"success");
-
-        result.data = products2;
+        result.data = products;
         result.total = total;
         res.send(result);
     }
     // Kim Ju Hui : 2019.08.30 Fri Fin-------------------------------------------
     // Kim Ju Hui : 2019.09.17 Tue-------------------------------------------
-    static getFileUrl = async(req, res) => {
+    static getFileUrl = async (req, res) => {
 
         console.log(req.query);
-        const {prefix, fileName, extension} = req.query;
+        const {fileName, fileExtension, fileType} = req.query;
+        // Kim Ju Hui : 2019.09.29 Sun ------------------------------------------
+        let folderName = '';
 
-        //const file = require('fs').createWriteStream('test.png');
-
-        // let prefix = 'P_File/';
-        // let key = '1.png';
-
-        console.log('P_File/'+fileName+'.'+extension);
-
-        let getParams = {
-            Bucket : 'diginalog-s3',
-            Key : prefix + fileName +'.'+extension,
-            Expires : 60 * 5
+        switch (fileType) {
+            case '0':
+                folderName = 'P_TitleIMG/';
+                break;
+            case '1':
+                folderName = 'P_DetailIMG/';
+                break;
+            case '2':
+                folderName = 'P_File/';
         }
 
-        /*let fileStream = s3.getObject(getParams).createReadStream();
-        res.attachment(name+'.'+extension);
-        fileStream.pipe(res);*/
+        let params = {
+            Bucket: 'diginalog-s3',
+            Key: folderName + fileName + '.' + fileExtension
+        }
 
-        const url = s3.getSignedUrl('getObject',getParams);
-        console.log(url);
-        res.send(url);
+        s3.headObject(params, function (err, matadata) {
+            if (err && err.code == 'NotFound') {
+                const url = 'error';
+                res.send(url);
+            } else {
+                const url = s3.getSignedUrl('getObject', params);
+                res.send(url);
+            }
+        })
+        // Kim Ju Hui : 2019.09.29 Sun Fin-------------------------------------
     }
     // Kim Ju Hui : 2019.09.17 Tue Fin-------------------------------------
 }
