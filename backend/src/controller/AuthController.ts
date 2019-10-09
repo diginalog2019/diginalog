ad318859651aab9e6b96748fe7a2c293bef57380
siraegi <shihabbong11@gmail.com>
2019-10-09 13:44:47 +0900

    JWT auth basic done

--- backend/src/controller/AuthController.ts
+++ backend/src/controller/AuthController.ts
@@ -8,7 +8,7 @@ export class AuthController {
         const {id, password} = req.body;
         let userinfo = {id, password};
         let user = await getConnection().getRepository(User).findOne({where: {U_ID: id}});
-        if(user) {
+        if (user) {
             console.log("exist user id");
         } else {
             user = await AuthController.addUser(userinfo);
@@ -36,6 +36,50 @@ export class AuthController {
         return newUser;
     }
     static login = async (req, res) => {
+        const {id, password} = req.body;
+        const secret = req.app.get('jwt-secret');
 
+        let user = await getConnection().getRepository(User).findOne({where: {U_ID: id}});
+
+        const response = (token) => {
+            res.json({
+                message: 'logged in successfully',
+                token
+            })
+        }
+        if (!user) {
+            console.log("login failed. user does not exist");
+        } else {
+            if(user.U_PW == password) {
+                // create a promise that generates jwt asynchronously
+                const p = new Promise((resolve, reject) => {
+                    jwt.sign({
+                        id: user.U_ID,
+                        name: user.U_Name
+                        },
+                        secret,
+                        {
+                            expiresIn: '7d',
+                            issuer: 'diginalog',
+                            subject: 'userInfo'
+
+                        }, (err, token) => {
+                            if(err) reject(err);
+                            resolve(token);
+                        })
+                }).then(response);
+            } else {
+                console.log("pw not match");
+            }
+        }
+        // console.log 대신 result에 에러메시지를 넘겨주면 좋을듯?
+        //const result = new ResultVo(0, "success");
+
+    }
+    static check = async (req, res) => {
+        res.json({
+            success: true,
+            info: req.decoded
+        })
     }
 }
--- backend/src/router/auth.ts
+++ backend/src/router/auth.ts
@@ -1,7 +1,11 @@
 import {Router} from "express";
 import {AuthController} from "../controller/AuthController"
 const routes = Router();
+const authMiddleware = require('../controller/auth');
 
 routes.post('/register', AuthController.register);
 routes.post('/login', AuthController.login);
+routes.use('/check', authMiddleware);
+routes.get('/check', AuthController.check);
+
 export default routes;
